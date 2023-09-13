d1ff4d2d24d63c9f0bd0bed231776085e12ef534
hernanc77 <hernancarballo@hotmail.com>
2023-09-13 19:42:09 -0300

    MoreWine

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,14 @@
+import carro from "./assets/img/carro.svg";
+
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src={carro} alt="Carro_img" />
+            0
+        </div>
+    );
+
+}
+
+export default CartWidget;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,37 @@
+import CartWidget from "../CartWidget/CartWidget";
+import Favicon from "./assests/img/favicon.png";
+import "bulma/css/bulma.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+  <img src={Favicon} alt="favicon" />
+    <h4>MoreWine</h4>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Vinos Tintos</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Vinos Blancos</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Vinos Espumantes</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+       
+    );
+}
+
+export default NavBar;
